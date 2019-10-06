5d2d131ee354824971f3f2340b5899c6244ae5a6
pworontzoff <pierre.worontzoff@hepl.be>
2019-10-06 10:09:13 +0200

    Add files via upload

---
+++ ant/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "paper.h"
+
+int main()
+{
+    init_paper(5,5,25,0.25,0);
+    repeat(9) {
+        if (get_color()==white) {
+            change_color(black);
+        } else {
+            change_color(white);
+        }
+        colorize();
+        move_right();
+        colorize();
+        move_down();
+        colorize();
+        move_left();
+        colorize();
+        move_up();
+    } loop;
+    display_paper();    
+    return 0;
+}
